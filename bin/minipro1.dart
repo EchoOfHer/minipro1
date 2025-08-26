70ebd7aa2358ce7110b1573df48beb4da3066f92
lukpeachhh <6631501022@lamduan.mfu.ac.th>
2025-08-26 16:51:11 +0700

    Done Final Function Adding

--- bin/minipro1.dart
+++ bin/minipro1.dart
@@ -21,7 +21,7 @@ Future<void> addExpense(int userId) async {
     return;
   }
 
-  final url = Uri.parse("http://localhost:3000/addexpense");
+  final url = Uri.parse("http://localhost:3000/addexpenses");
 
   var response = await http.post(
     url,
